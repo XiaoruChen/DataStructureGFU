317992cc790f03d167f32580f0c680fcfe3411a2
XiaoruChen <115509557+XiaoruChen@users.noreply.github.com>
2022-10-10 16:23:08 -0700

    Assignment 8

---
+++ Assignment8/HashFunction.java
@@ -0,0 +1,13 @@
+package hash_function;
+
+public class HashFunction
+{
+
+   public static void main(String[] args)
+   {
+      int x = 54;
+      System.out.println((2 * x + 5) % 12);
+
+   }
+
+}
